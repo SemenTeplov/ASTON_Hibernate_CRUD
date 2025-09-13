84d1c7f01721b9c32fb086b071ac72d33a1abec9
SemenTeplov <eburg-severca@mail.ru>
2025-09-13 10:34:43 +0500

    fixing

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
@@ -11,11 +12,10 @@ import java.time.LocalDate;
 @Data
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "Users")
 public class User {
-    public User() {}
-
     @Id
     private int id;
 
--- src/main/java/repositories/UsersRepository.java
+++ src/main/java/repositories/UsersRepository.java
@@ -7,61 +7,69 @@ import lombok.extern.slf4j.Slf4j;
 
 import models.User;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Slf4j
 public class UsersRepository implements Repository {
-    private Session session;
     private Transaction transaction;
 
     @Override
     public void create(@NonNull User user) {
-        open();
-        session.save(user);
-        close();
+        try (Session session = Datebase.getSession()) {
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
 
-        log.info("User created");
+            log.info("User created");
+        } catch (HibernateException e) {
+            log.error("Session was fall, exception: {}", e.getMessage());
+        }
     }
 
     @Override
     public User read(int id) {
         User user;
 
-        open();
-        user = session.get(User.class, id);
-        close();
+        try (Session session = Datebase.getSession()) {
+            transaction = session.beginTransaction();
+            user = session.get(User.class, id);
+            transaction.commit();
 
-        log.info("Got user");
+            log.info("Got user");
 
-        return user;
+            return user;
+        } catch (HibernateException e) {
+            log.error("Session was fall, exception: {}", e.getMessage());
+        }
+
+        return null;
     }
 
     @Override
     public void update(@NonNull User user) {
-        open();
-        session.update(user);
-        close();
+        try (Session session = Datebase.getSession()) {
+            transaction = session.beginTransaction();
+            session.update(user);
+            transaction.commit();
 
-        log.info("User updated");
+            log.info("User updated");
+        } catch (HibernateException e) {
+            log.error("Session was fall, exception: {}", e.getMessage());
+        }
     }
 
     @Override
     public void delete(@NonNull User user) {
-        open();
-        session.delete(user);
-        close();
+        try (Session session = Datebase.getSession()) {
+            transaction = session.beginTransaction();
+            session.delete(user);
+            transaction.commit();
 
-        log.info("User deleted");
-    }
-
-    private void open() {
-        session = Datebase.getSession();
-        transaction = session.beginTransaction();
-    }
-
-    private void close() {
-        transaction.commit();
-        session.close();
+            log.info("User deleted");
+        } catch (HibernateException e) {
+            log.error("Session was fall, exception: {}", e.getMessage());
+        }
     }
 }
